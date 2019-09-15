1e17d8c1895c44dab7ebdc0c3e3e206986a03d3a
Duckwall <jake.duckwall@pm.me>
2019-09-15 12:36:48 -0400

    Add template file

---
+++ webapp_fuzzer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import argparse
+
+
+
+def main():
+
+
+
+
+if __name__ == '__main__':
+    
+    parser = argparser.ArgumentParser()
+    parser.add_argument(
+        "positional",
+        help=""
+    )
+    args = parser.parse_args()
+
+    main()
